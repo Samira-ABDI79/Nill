cf9dd844c417aa0350549371306bace7b0e09e00
Samira-ABDI79 <samirabdiurmia@gmail.com>
2022-11-21 06:48:51 -0800

    feat (Home) : add site name

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -30,6 +30,43 @@ function Home(){
   </div>
      </div>
      </div>
+
+     {/* ********* finish firset component ---texst's and image's */}
+
+     <div className="container bg-black ">
+         <div className="">
+         <div className="container  flex flex-col lg:flex-row md:flex-row mt-10 justify-between">
+     <div>
+         <img src={images['Home/image 15.png']} alt="طراحی ست اداری" className="w-3/7 " />
+        
+
+     </div>
+     <div dir="rtl" className=" my-12 lg:my-0 md:my-0 ">
+      <div className="lg:w-4/6">
+      <p className="text-white font-bold text-4xl lg:text-3xl "> طراحی ست اداری شرکت hydeout</p>
+        <p className="text-gray-dark  font-bold text-2xl lg:text-xl mt-10">نتلنم نمفتثقن ثخصجح نتبی تثنقتف تقفتقه 
+            نتلنقتت5فخهت
+            نتلخقتلخت تقفتق نقتقنتفقخحک فف ق قق نکقنف صثقتثخحفخ نتفقخغخ 
+            تنقانقمتا قفنتقنفت قنتقثفت ققنفتن 
+        </p>
+        <button className="bg-blue  text-white h-8 mt-12 hover:opacity-75 rounded px-12 font-semibold py-6 flex justify-center items-center">
+  <Link to="">بیشتر</Link>
+  </button>
+      </div>
+  </div>
+     </div>
+         </div>
+     
+     </div>
+
+{/* ************************* */}
+
+<div className="container bg-white ">
+     <div className="container flex flex-col lg:flex-row md:flex-row">
+    <div> </div>
+     </div>
+     </div>
+
      <EmployersComments />
      <ClosedProjects />
 <NewProject />
