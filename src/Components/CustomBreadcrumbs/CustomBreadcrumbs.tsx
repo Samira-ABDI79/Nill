f0364f9d44048b8d84c7d42605f856614f8d4b9f
Samira-ABDI79 <samirabdiurmia@gmail.com>
2022-11-23 07:22:52 -0800

    feat ( Nill) : update CustomBreadcrumbs

--- src/Components/CustomBreadcrumbs/CustomBreadcrumbs.tsx
+++ src/Components/CustomBreadcrumbs/CustomBreadcrumbs.tsx
@@ -24,9 +24,28 @@ interface CustomBreadcrumbsProps {
           
            
             <div className="flex items-center flex-row mt-8">
-        <div className=" h-[10px]  w-[10px] bg-slate-300   rounded-full"></div>
+        {/* <div className=" h-[10px]  w-[10px] bg-slate-300   rounded-full"></div>
             
-        <div className="h-[1px]  w-10 bg-slate-300 "></div>
+        <div className="h-[1px]  w-10 bg-slate-300 "></div> */}
+        <div>
+                <svg
+                  width="56"
+                  height="8"
+                  viewBox="0 0 56 8"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle cx="52" cy="4" r="4" fill="#D0D0D0" />
+                  <line
+                    x1="49.5"
+                    y1="4.5"
+                    x2="0.5"
+                    y2="4.5"
+                    stroke="#D0D0D0"
+                    stroke-linecap="round"
+                  />
+                </svg>
+              </div>
 
         <p className="mr-2 text-slate-300 text-sm">
           
--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -6,7 +6,7 @@ export { default as Teammate } from "./Teammate/Teammate";
 export { default as About } from "./About/About";
 export { default as ScrollToTop } from "./ScrollToTop/ScrollToTop";
 export { default as Button } from "./Button/Button";
-export { default as Ellipse } from "./Ellipse/Ellipse";
+
 
 
 
