476e83054499705d8b5384c2e9ed48ae7cf6120b
Samira-ABDI79 <samirabdiurmia@gmail.com>
2022-11-27 06:59:35 -0800

    feat (Nilll) : update style

--- src/Services/Data/data.ts
+++ src/Services/Data/data.ts
@@ -1,36 +1,7 @@
 
 import images from "../../Importer/Importer"
 // ------------------------- start navbar data ---------------
-export const navbarItems = [
-  
-  {
-    id: 1,
-    name: "About us",
-    route: "/about",
-  },
-  {
-    id: 2,
-    name: "Blog",
-    route: "/blog",
-  },
-  {
-    id: 3,
-    name: "Work",
-    route: "/work",
-  },
-  {
-    id: 4,
-    name: "Post",
-    route: "/post",
-  },
-  {
-    id: 5,
-    name: "Teammate",
-    route: "/teammate",
-  },
 
-];
-// -------------------------end navbar data ---------------
 // ------------------------- start menu data ---------------
 export const menuItems = [
   
