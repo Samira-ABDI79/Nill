66f12f50680c8087e8cc8c144cd0c486d09ed81a
Samira-ABDI79 <samirabdiurmia@gmail.com>
2022-11-29 01:01:47 -0800

    feat (Blog) :add useWindowSize in slider

--- src/Components/Blog/Components/BlogSlider.tsx
+++ src/Components/Blog/Components/BlogSlider.tsx
@@ -3,8 +3,19 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import "./BlogSlider.css"
 import {BlogsData} from "../data"
 import Article from "./Article";
-import { useState } from "react";
-
+import React, { useLayoutEffect, useState } from 'react';
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/grid';
@@ -12,27 +23,25 @@ import SwiperCore,{Grid,Pagination} from "swiper"
 SwiperCore.use([Pagination])
 function BlogSlider(){
   const [articleData]=useState(BlogsData);
-
+  const [width, height] = useWindowSize();
+  let rowsCount =6;
+  if(width<=768){
+    rowsCount=1
+  }
+  else {
+    rowsCount=6;
+  }
+  console.log(rowsCount)
     return (
         <>
           <Swiper
      modules={[Grid]}
      slidesPerView={1}
-     grid={{rows:6,fill:"row"}}
+     grid={{rows:rowsCount,fill:"row"}}
      spaceBetween={2}
      pagination={{clickable:true}}
      className="BlogSlider "
-     breakpoints={{
-      50:{
-        // grid:{rows:2,fill:"row"},
-        // rows:2
-      },
-      900:{
-        // grid:{rows:6,fill:"row"}
-
-    }}}
-  
-    
+     
     >
 
 {articleData.map((item) => (
