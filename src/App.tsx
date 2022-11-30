79bb11c4eb331e4d73ab3d20e9323ef70be88ed1
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2022-11-30 18:43:08 +0330

    add aos to project & comment aos & GSAP

--- src/App.tsx
+++ src/App.tsx
@@ -18,38 +18,76 @@ import {
 } from "./Pages";
 
 import { MainLayout, HomeLayout, PostLayout } from "./Layouts";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
 import "./App.css";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function App() {
-  const mainRef = useRef(null);
-  const scrollRef = useRef(null);
+  // START AOS plugin
+  // useEffect(() => {
+  //   AOS.init({
+  //     // Global settings:
+  //     disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  //     startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+  //     initClassName: "aos-init", // class applied after initialization
+  //     animatedClassName: "aos-animate", // class applied on animation
+  //     useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  //     disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  //     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  //     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
-  function setHeight(mainRef) {
-    const target = mainRef.current;
-    const headerHeight = document.getElementById("header").offsetHeight;
-    target.style.marginTop = `${headerHeight}px`;
-  }
+  //     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  //     offset: 120, // offset (in px) from the original trigger point
+  //     delay: 0, // values from 0 to 3000, with step 50ms
+  //     duration: 400, // values from 0 to 3000, with step 50ms
+  //     easing: "ease", // default easing for AOS animations
+  //     once: true, // whether animation should happen only once - while scrolling down
+  //     mirror: false, // whether elements should animate out while scrolling past them
+  //     anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+  //   });
+  //   AOS.refresh();
+  // });
 
-  // routing scrollTrigger refresh
-  useEffect(() => {
-    ScrollTrigger.refresh();
-    // setTimeout(() => {
-    //   ScrollTrigger.refresh();
-    // }, 100);
-  });
 
-  useEffect(() => {
-    setHeight(mainRef);
-    smoothScroll(scrollRef);
-    window.addEventListener("resize", () => {
-      setHeight(mainRef);
-    });
-  }, []);
+  // START GSAP plugin
+  // const mainRef = useRef(null);
+  // const scrollRef = useRef(null);
+
+  // function setHeight(mainRef) {
+  //   const target = mainRef.current;
+  //   const headerHeight = document.getElementById("header").offsetHeight;
+  //   target.style.marginTop = `${headerHeight}px`;
+  // }
+
+  // // routing scrollTrigger refresh
+  // useEffect(() => {
+  //   ScrollTrigger.refresh();
+  //   // setTimeout(() => {
+  //   //   ScrollTrigger.refresh();
+  //   // }, 100);
+  // });
+
+  // useEffect(() => {
+  //   setHeight(mainRef);
+  //   smoothScroll(scrollRef);
+  //   window.addEventListener("resize", () => {
+  //     setHeight(mainRef);
+  //   });
+  // }, []);
+  // pedar id="header" className="scroll_wrap"
+  // child  className="scroll_container" ref={scrollRef}
+  // ref={mainRef}
+
+
+  // some child aos animation has attr: 
+  //  data-aos="fade-up"
+  // data-aos-delay="50"
+  // data-aos-duration="900"
   return (
-    <div id="header" className="scroll_wrap">
-      <div className="scroll_container" ref={scrollRef}>
-        <main ref={mainRef}>
+    <div>
+      <div>
+        <main >
           <HashRouter>
             <ScrollToTop>
               <Routes>
